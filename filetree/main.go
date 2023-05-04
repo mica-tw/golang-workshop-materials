0401053cb936a1ce44584a8594e77118faf0129e
Mica Beneke <127195973+mica-tw@users.noreply.github.com>
2023-05-04 15:51:00 +0200

    Use command line arguments

--- filetree/main.go
+++ filetree/main.go
@@ -4,20 +4,29 @@ import (
 	"filetree/tree"
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 func main() {
-	root := "."
+	var root string
+
+	switch i := len(os.Args); i {
+	case 1:
+		root = "."
+	case 2:
+		root = os.Args[1]
+	default:
+		fmt.Printf("You supplied %d arguments, a maximum of 1 is allowed", i-1)
+		os.Exit(1)
+	}
 
 	fileTree := tree.Tree{
 		FileName: root,
 	}
 	filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		fileTree.AddToTree(strings.Split(path, "/"))
-
-		// fmt.Printf("tree after adding '%s' to it: %v\n", path, fileTree)
 
 		return nil
 	})
